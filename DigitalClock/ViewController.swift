cfa9eecbfa9732cafa788bfb8e8658d5a667991f
iFlame <iflame@administrators-device.local>
2017-05-15 12:26:41 +0530

    his

--- DigitalClock/ViewController.swift
+++ DigitalClock/ViewController.swift
@@ -9,17 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+   
+    @IBOutlet weak var Label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    Timer.scheduledTimer(timeInterval: 0.1, target: self, selector:#selector(ViewController.updateTime), userInfo: nil, repeats: true)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
-}
+    func updateTime(){
+        Label.text = DateFormatter.localizedString(from: Date(), dateStyle: DateFormatter.Style.none, timeStyle: DateFormatter.Style.full)
+    }
+    
+    
+       
+ }
 
